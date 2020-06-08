6269cd0c5527196a764bae47ef6e5b28d7106f1d
litleleprikon <e.sharifullin@skbkontur.ru>
2020-06-08 17:40:04 +0300

    feat(datatype): Add team type
    Add moira.Team datatype that and it representation in redis.
    This datatype represents team of users that can share contacts and
    subscriptions.
    
    Relates #230

--- datatypes.go
+++ datatypes.go
@@ -172,6 +172,12 @@ func (trigger TriggerData) GetTriggerURI(frontURI string) string {
 	return ""
 }
 
+// Team is a structure that represents a group of users that share a subscriptions and contacts
+type Team struct {
+	Name        string
+	Description string
+}
+
 // ContactData represents contact object
 type ContactData struct {
 	Type  string `json:"type"`
